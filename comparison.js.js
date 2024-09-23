da77ead497021e82d8cd84636ad31531a029f44f
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-23 17:51:48 +0800

    Add files via upload

---
+++ comparison.js.js
@@ -0,0 +1,14 @@
+// Comparison or Relational Operator
+var firstFavNumb = 11;
+var secondFavNumb = 21;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
+console.log("firstFavNumb:", firstFavNumb);
+console.log("secondFavNumb:", secondFavNumb);
